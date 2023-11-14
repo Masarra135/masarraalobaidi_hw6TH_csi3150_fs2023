fb9fbc99217f3da7ce52ab8c953d74caa15192ec
Masarra135 <71594188+Masarra135@users.noreply.github.com>
2023-11-14 13:34:11 -0500

    getting react started

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,16 @@
+import React from "react";
+function HelloWorld() {
+  const imageSource = "";
+  return (
+    <div className="parent-container">
+      <h1>Friends</h1>
+      <h2>
+        Friends is an American television sitcom created by David Crane and
+        Marta Kauffman, which aired on NBC from September 22, 1994, to May 6,
+        2004, lasting ten seasons.
+      </h2>
+      <img src="" alt=""></img>
+    </div>
+  );
+}
+export default HelloWorld;
